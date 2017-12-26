124159287d32bda45a1802f36abdc8a1183f12ee
Thamizharasu <zarub2k@gmail.com>
2017-12-26 21:24:14 +0530

    initial version is committed

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:darthustle/darthustle.dart' as darthustle;
+
+main(List<String> arguments) {
+  print('Hello world: ${darthustle.calculate()}!');
+}
---
+++ test/darthustle_test.dart
@@ -0,0 +1,8 @@
+import 'package:darthustle/darthustle.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
