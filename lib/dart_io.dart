fdeea3e21b7aab44f1945d245a9869b37923b89b
Thamizharasu <zarub2k@gmail.com>
2017-12-28 23:31:08 +0530

    file path changed

--- lib/dart_io.dart
+++ lib/dart_io.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 Future readFile() async {
   try {
-    var file = new File("C:\\work\\personal\\repos\\public\\darthustle\\lib\\hello.json");
+    var file = new File("hello.json");
     var content = await file.readAsLines();
     print("Content of the file is $content");
   } catch (e, s) {
