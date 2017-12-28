cb35367b42401abb1e9dbc0dedb4cc3c137c5d35
Thamizharasu <zarub2k@gmail.com>
2017-12-29 00:09:40 +0530

    response read from http response

--- lib/http_client.dart
+++ lib/http_client.dart
@@ -1,15 +1,18 @@
 import 'dart:io';
 import 'dart:async';
+import 'dart:convert';
 
 Future main() async {
   var uri = Uri.parse('http://localhost:9200/');
 
   var client = new HttpClient();
   var request = await client.getUrl(uri);
-  var response = await request.close();
+  HttpClientResponse response = await request.close();
   
   print(response.statusCode);
   print(response.headers);
+
+  response.transform(UTF8.decoder).listen((data) => print(data));
 
   client.close();
 }
