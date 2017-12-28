5a908e7d5b2c4fd6edfc11e06835b8fd091a7725
Thamizharasu S <zarub2k@gmail.com>
2017-12-28 16:50:57 +0530

    IO package work started

--- lib/dart_io.dart
+++ lib/dart_io.dart
@@ -2,5 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 Future readFile() async {
-  new File()
+  var file = new File("hello.json");
+  var content = await file.readAsString();
+  print("Content of the file is $content");
 }
---
+++ test/dart_io_test.dart
@@ -0,0 +1,8 @@
+import 'package:darthustle/dart_io.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('readFile', () {
+    readFile();
+  });
+}
