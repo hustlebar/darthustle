18e8026451039839a9a276720c95232f982668a8
Thamizharasu S <zarub2k@gmail.com>
2017-12-29 14:23:16 +0530

    Collection example tried

--- bin/core.dart
+++ bin/core.dart
@@ -1,5 +1,37 @@
 void main() {
-  dateTime();
+  collection();
+}
+
+void collection() {
+  //List
+  List players = new List<String>();
+  players.addAll(["Roger", "Paes", "Nadal", "Joko"]);
+  players.add("Roger");
+  players.forEach((player) => print(player));
+
+  players.removeLast();
+  print(players);
+
+  players.sort((a, b) => a.compareTo(b));
+  print(players);
+
+  //Set
+  var langs = new Set<String>();
+  langs.addAll(["Java", "Dart", "Swift", "Android"]);
+  langs.forEach((lang) => print(lang));
+
+  //Map
+  var persons = new Map();
+  persons.addAll({
+    "Tham": "Chennai",
+    "Roger": "Swizerland",
+    "Nadal": "Spain",
+    "Paes": "India"
+  });
+
+  persons.forEach((key, value) => print('$key, $value'));
+  persons.putIfAbsent("Joko", ()=> "Serbia");
+  persons.forEach((key, value) => print('$key, $value'));
 }
 
 void dateTime() {
