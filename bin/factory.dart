171c461e1d8d6d8ff730f3b907fbaffbdfa56033
Thamizharasu <zarub2k@gmail.com>
2018-01-01 13:12:25 +0530

    Clean up is done

--- bin/factory.dart
+++ bin/factory.dart
@@ -1,19 +1,43 @@
+import 'dart:math';
+
 abstract class Shape {
   factory Shape(String type) {
     if (type == 'Circle') {
-      return new Circle();
-    } else if (type == "Rectangle") {
-      return new Rectangle();
+      return new Circle(2);
+    } else if (type == "Square") {
+      return new Square(2);
     }
 
     throw 'Type is not identified';
   }
+
+  num get area;
 }
 
 class Circle implements Shape {
+  final num radius;
+  Circle(this.radius);
 
+  @override
+  num get area => PI * pow(radius, 2);
 }
 
-class Rectangle implements Shape {
+class Square implements Shape {
+  num side;
 
+  Square(this.side);
+
+  @override
+  num get area => pow(side, 2);
+}
+
+void main() {
+  var circle = new Shape('Circle');
+  print(circle.area);
+
+  var square = new Shape('Square');
+  print(square.area);
+
+  var rect = new Shape('Rectangle');
+  print(rect.area);
 }
